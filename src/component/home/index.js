fd8706524abb0c8a215827d50c7df79c10fd98b9
shaimaamoamen <shaimaamoamen86@gmail.com>
2022-12-19 20:13:52 +0200

    modifiy3

--- src/component/home/index.js
+++ src/component/home/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import Carousel from 'react-bootstrap/Carousel';
 import './Style.css'
-import Picture3 from "./../../Images/1.png"
+import Picture3 from "./../../Images/3.jpg"
 import Picture4 from "./../../Images/4.jpg"
 import Picture5 from "./../../Images/5.jpg"
 import Picture6 from "./../../Images/6.jpg"
